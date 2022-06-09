641c76096e007b65c41cbc404fb87ae7ccfa1189
Edsey omenda <omendajames0317@gmail.com>
2022-06-09 17:14:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const myName = upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+
+
+const best =  setBestCustomer = () => bestCustomer = 'not bob';
+
+const  o_custumer = overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'James';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'perhaps bob';
+  console.log(leastFavoriteCustomer);
+}
+changeLeastFavoriteCustomer();
